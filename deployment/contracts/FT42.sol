aa1e35c5a1a4922aa0ae80d8beda77869a1f743e
Naerhy <naerhy@protonmail.com>
2024-03-07 23:14:44 +0100

    Add function to enable or disable transfers

--- deployment/contracts/FT42.sol
+++ deployment/contracts/FT42.sol
@@ -5,7 +5,23 @@ import { ERC20 } from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
 
 contract FT42 is ERC20, Ownable {
+	bool public transferStatus = true;
+
 	constructor(uint256 initialSupply) ERC20("FT42", "FT42") Ownable(_msgSender()) {
 		_mint(msg.sender, initialSupply);
+	}
+
+	function switchTransferStatus() public onlyOwner {
+		transferStatus = !transferStatus;
+	}
+
+	function transfer(address to, uint256 value) public override returns (bool) {
+		require(transferStatus, "transfer is disabled");
+		return super.transfer(to, value);
+	}
+
+	function transferFrom(address from, address to, uint256 value) public override returns (bool) {
+		require(transferStatus, "transfer is disabled");
+		return super.transferFrom(from, to, value);
 	}
 }
--- deployment/test/FT42.ts
+++ deployment/test/FT42.ts
@@ -6,18 +6,51 @@ describe("FT42", function () {
 	async function deployFT42Fixture() {
 		const INITIAL_SUPPLY = 10000000;
 		const [owner, account1, account2] = await ethers.getSigners();
-
 		const factory = await ethers.getContractFactory("FT42");
 		const contract = await factory.deploy(ethers.parseEther(INITIAL_SUPPLY.toString()));
-
 		return { contract, owner, account1, account2 };
 	}
 
-	describe("Deployment", function () {
-		it("Should set the right owner", async function () {
+	describe("Deployment", function() {
+		it("deployer address should be owner by default", async function() {
 			const { contract, owner } = await loadFixture(deployFT42Fixture);
-
 			expect(await contract.owner()).to.equal(owner.address);
+		});
+
+		it("transferStatus should be enabled by default", async function() {
+			const { contract } = await loadFixture(deployFT42Fixture);
+			expect(await contract.transferStatus()).to.equal(true);
+		});
+
+		it("transferStatus should be disabled after switch", async function() {
+			const { contract } = await loadFixture(deployFT42Fixture);
+			await contract.switchTransferStatus();
+			expect(await contract.transferStatus()).to.equal(false);
+		});
+
+		it("transferStatus should be enabled after switching twice", async function() {
+			const { contract } = await loadFixture(deployFT42Fixture);
+			await contract.switchTransferStatus();
+			await contract.switchTransferStatus();
+			expect(await contract.transferStatus()).to.equal(true);
+		});
+
+		it("transferStatus should not be switched by another account than owner", async function() {
+			const { contract, account1 } = await loadFixture(deployFT42Fixture);
+			await expect(contract.connect(account1).switchTransferStatus()).to.be.reverted;
+		});
+
+		it("transfer should be reverted", async function() {
+			const { contract, account1 } = await loadFixture(deployFT42Fixture);
+			await contract.switchTransferStatus();
+			await expect(contract.transfer(account1, ethers.parseEther("1000"))).to.be.reverted;
+		});
+
+		it("transfer should not be reverted", async function() {
+			const { contract, account1 } = await loadFixture(deployFT42Fixture);
+			await contract.switchTransferStatus();
+			await contract.switchTransferStatus();
+			await expect(contract.transfer(account1, ethers.parseEther("1000"))).not.to.be.reverted;
 		});
 	});
 });
